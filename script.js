3bcdb466adf1c87bd3dd79368d573352bb990320
yorik <yorikyorik25@gmail.com>
2021-06-01 22:12:30 +0300

    button start

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
 var arrSuit = ["&#9829", "&#9830", "&#9827", "&#9824"]
-
+var switchVar = 0
+var check = document.getElementById("inp")
 var tern = document.getElementsByClassName("tern")
 var allside = document.getElementsByClassName("allside")
 
+check.addEventListener("click", function(){
+    if (switchVar === 0){
+        switchVar = 1
+        for (var i = 0; i < 6; i++){
+            var temp = Math.floor(Math.random() * 4)
 
-function start(){
-    for (var i = 0; i < 6; i++){
-        var temp = Math.floor(Math.random() * 4)
+            allside[i].innerHTML = arrSuit[temp]
 
-        allside[i].innerHTML = arrSuit[temp]
-
-        if (arrSuit[temp] == "&#9829" ||
-            arrSuit[temp] == "&#9830") {
-            allside[i].style.color = "red"
-        }else{
-            allside[i].style.color = "black"
+            if (arrSuit[temp] == "&#9829" ||
+                arrSuit[temp] == "&#9830"){
+                allside[i].style.color = "red"
+            }else{
+                allside[i].style.color = "black"
+            }
         }
-    }
 
-    for (var i = 0; i < 3; i++){
-        tern[i].style.transform = "rotateX(7200deg)"
-    }
-}
+        for (var i = 0; i < 3; i++){
+            tern[i].style.transform = "rotateX(7200deg)"
+        }
+    }return
+
+    if (switchVar === 1){
+        switchVar = 0
+        for (var i = 0; i < 6; i++){
+            var temp = Math.floor(Math.random() * 4)
+
+            allside[i].innerHTML = arrSuit[temp]
+
+            if (arrSuit[temp] == "&#9829" ||
+                arrSuit[temp] == "&#9830"){
+                allside[i].style.color = "red"
+            }else{
+                allside[i].style.color = "black"
+            }
+        }
+
+        for (var i = 0; i < 3; i++){
+            tern[i].style.transform = ""
+        }
+    }return
+})
 
