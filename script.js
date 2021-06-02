db8fb9e92f26452fd1b2a3f99d2742c90efbc9ee
yorik <yorikyorik25@gmail.com>
2021-06-02 12:51:35 +0300

    balans

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var arrSuit = ["&#9829", "&#9830", "&#9827", "&#9824"]
-var switchVar = 0
 var check = document.getElementById("inp")
 var tern = document.getElementsByClassName("tern")
 var allside = document.getElementsByClassName("allside")
+var front = document.getElementsByClassName("front")
+var balanсe = 1000
+var switchVar = 0
+document.getElementById('data').innerHTML = balanсe;
 
 check.addEventListener("click", function(){
+
     if (switchVar === 0){
         switchVar = 1
         for (var i = 0; i < 6; i++){
@@ -19,11 +23,17 @@ check.addEventListener("click", function(){
                 allside[i].style.color = "black"
             }
         }
+       /* if (front[0].textContent === front[1].textContent
+            && front[1].textContent === front[2].textContent){
+            balance += 50;
+        } else {
+            balance -= 25;
+        }*/
 
         for (var i = 0; i < 3; i++){
             tern[i].style.transform = "rotateX(7200deg)"
         }
-    }else {
+    }   else {
 
     if (switchVar === 1){
         switchVar = 0
@@ -45,5 +55,7 @@ check.addEventListener("click", function(){
         }
     }
     }
+
+    //document.getElementById('data').innerHTML = balanсe;
 })
 
