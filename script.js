c6b635358313cafb0938c5a60404316f2c54c327
yorik <yorikyorik25@gmail.com>
2021-06-02 20:26:19 +0300

    balans 2

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ var check = document.getElementById("inp")
 var tern = document.getElementsByClassName("tern")
 var allside = document.getElementsByClassName("allside")
 var front = document.getElementsByClassName("front")
-var balanсe = 1000
 var switchVar = 0
+
+var balanсe = 1000
 document.getElementById('data').innerHTML = balanсe;
+//var bal = balanсe+100;
+//document.getElementById('datta').innerHTML = bal;
+
 
 check.addEventListener("click", function(){
 
@@ -23,39 +27,36 @@ check.addEventListener("click", function(){
                 allside[i].style.color = "black"
             }
         }
-       /* if (front[0].textContent === front[1].textContent
-            && front[1].textContent === front[2].textContent){
-            balance += 50;
-        } else {
-            balance -= 25;
-        }*/
-
         for (var i = 0; i < 3; i++){
             tern[i].style.transform = "rotateX(7200deg)"
         }
-    }   else {
+    }else {
 
-    if (switchVar === 1){
-        switchVar = 0
-        for (var i = 0; i < 6; i++){
-            var temp = Math.floor(Math.random() * 4)
+        if (switchVar === 1){
+            switchVar = 0
+            for (var i = 0; i < 6; i++){
+                var temp = Math.floor(Math.random() * 4)
 
-            allside[i].innerHTML = arrSuit[temp]
+                allside[i].innerHTML = arrSuit[temp]
 
-            if (arrSuit[temp] == "&#9829" ||
-                arrSuit[temp] == "&#9830"){
-                allside[i].style.color = "red"
-            }else{
-                allside[i].style.color = "black"
+                if (arrSuit[temp] == "&#9829" ||
+                    arrSuit[temp] == "&#9830"){
+                    allside[i].style.color = "red"
+                }else{
+                    allside[i].style.color = "black"
+                }
+            }
+
+            for (var i = 0; i < 3; i++){
+                tern[i].style.transform = ""
             }
         }
-
-        for (var i = 0; i < 3; i++){
-            tern[i].style.transform = ""
-        }
     }
+    if (front[0].textContent === front[1].textContent
+        && front[1].textContent === front[2].textContent){
+        balance += 50
     }
 
-    //document.getElementById('data').innerHTML = balanсe;
-})
+});
+document.getElementById('data').innerHTML = balanсe;
 
